a40fdf9f2a920a7655dcdeea813e9b7c0d60e27c
Charles Choi <kickingvegas@gmail.com>
2025-01-16 20:04:03 -0800

    Misc updates
    - Update documentation
    - Added repo directories to exclude
    - Add version
    - Verify for POSIX.1 compliance

--- recent-rgrep.sh
+++ recent-rgrep.sh
@@ -15,27 +15,41 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
+VERSION=0.1.0
+
 ## Requirements
-# The following commands should be installed and made available to this script.
-# - GNU grep
-# - GNU coreutils (ls, mktemp)
-# - GNU awk
-# - xargs
+# This script presumes that GNU tools are being used, but should also be
+# compliant with their counterparts on macOS. Inspect your PATH variable to
+# confirm which variant you are using.
+AWK=awk
+GREP=grep
+MKTEMP=mktemp
+LS=ls
+XARGS=xargs
+
+## macOS configuration using explicit paths.
+# AWK=/usr/bin/awk
+# GREP=/usr/bin/grep
+# MKTEMP=/usr/bin/mktemp
+# LS=/bin/ls
+# XARGS=/usr/bin/xargs
 
 function usage {
     cat <<EOF >&2
 Usage: $(basename "$0") [OPTION]… [REGEXP]
 
-Recursively search for GNU grep REGEXP pattern and present results in order of
+Recursively search for grep REGEXP pattern and present results in order of
 most recently modified files descending.
 
    -c         Case sensitive search (default case insensitive)
    -f GLOB    Search only files whose name matches glob pattern GLOB.
               If omitted then all non-binary files will be searched.
+              GLOB should be quoted to avoid shell expansion.
    -h         Help
 
-This script requires that the GNU utilities grep, mktemp, xargs, ls, and awk are
-installed.
+This script requires that your local install of grep, awk, ls, mktemp, and xargs
+be compliant with IEEE Std 1003.1-2008 (“POSIX.1”). Please inspect your PATH
+variable to confirm which tool variant you are using.
 EOF
     exit 1
 }
@@ -44,7 +58,7 @@ EOF
 c=--ignore-case
 f=
 
-while getopts :f:ch optKey; do
+while getopts :f:chv optKey; do
     case $optKey in
 	f)
 	    f=--include="$OPTARG"
@@ -52,6 +66,11 @@ while getopts :f:ch optKey; do
 	c)
 	    c=
 	    ;;
+
+        v)
+            echo $VERSION
+            exit 0
+            ;;
 	h|*)
 	    usage
 	    ;;
@@ -65,21 +84,25 @@ if [ "$#" == 0 ]; then
     exit 1
 fi
 
-SORTED_FILES=$(mktemp --tmpdir)
+SORTED_FILES=$($MKTEMP --tmpdir)
 
 # Extract file names and sort by modification time
-grep --recursive                                \
+$GREP --recursive                                \
      -I                                         \
      --files-with-matches                       \
      $c                                         \
+     --exclude-dir=".git"                       \
+     --exclude-dir=".hg"                        \
+     --exclude-dir=".svn"                       \
+     --exclude-dir=".cvs"                       \
      $f                                         \
      --regexp="$1" |                            \
-    xargs ls -lt > $SORTED_FILES
+    $XARGS $LS -lt > $SORTED_FILES
 
 # Display the sorted results with matching lines
 while IFS= read -r file; do
-    grep --line-number --with-filename $c --regexp="$1" "$file"
-done < <(awk '{print $NF}' $SORTED_FILES)
+    $GREP --line-number --with-filename $c --regexp="$1" "$file"
+done < <($AWK '{print $NF}' $SORTED_FILES)
 
 # Clean up temporary file
 rm $SORTED_FILES
